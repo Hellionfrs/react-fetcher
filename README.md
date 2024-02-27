36e6de019e722892d057694dc6b6bccc09773840
fred <fredy.rodriguez@pucp.pe>
2024-02-26 22:01:26 -0500

    First aproach rendering cat img with the first three words

--- README.md
+++ README.md
@@ -2,12 +2,12 @@ Prueba
 
 APIs:
 
-Facts Random: https://catfact.ninja/fact
+- Facts Random: https://catfact.ninja/fact
 
-Imagen random: https://cataas.com/cat/says/hello
+- Imagen random: https://cataas.com/cat/says/hello
 
-Recupera un hecho aleatorio de gatos de la primera API
+- Recupera un hecho aleatorio de gatos de la primera API
 
-Recuperar la primera palabra del hecho
+- Recuperar la primera palabra del hecho
 
-Muestra una imagen de un gato con la primera palabra.
+- Muestra una imagen de un gato con la primera palabra.
--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,37 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+const CAT_ENDPOINT_RANDOM_FACT = "https://catfact.ninja/fact";
+
+const CAT_ENDPOINT_IMG_URL = `https://cataas.com/cat/says/`;
+
 export function App() {
-  return <div>App de gatitos!</div>
+  const [fact, setFact] = useState("cat fact");
+  const [firstWord, setFirstWord] = useState("first");
+  const [image, setImage] = useState(null);
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then((res) => res.json())
+      .then((data) => {
+        const { fact } = data;
+        setFact(fact);
+        const firstWord = fact.split(" ", 3).join(" ");
+        setFirstWord(firstWord);
+
+        setImage(`https://cataas.com/cat/says/${firstWord}`);
+      });
+  }, []);
+  return (
+    <main>
+      <h1>App de gatitos</h1>
+      <section>
+        {fact && <p>{fact}</p>}
+        {image && (
+          <img
+            src={image}
+            alt={`Img extracted using first word ${firstWord}`}
+          />
+        )}
+      </section>
+    </main>
+  );
 }
